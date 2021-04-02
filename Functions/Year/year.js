4c6e352b9944ee375fd51dfc63bd7a5b4fd34a71
McKynlee Westman <mckynlee.westman@gmail.com>
2021-04-02 11:28:06 -0500

    Leap year tests complete

--- Functions/Year/year.js
+++ Functions/Year/year.js
@@ -2,7 +2,21 @@
 // true if leap year, else false
 
 function year(inputYear) {
-  return 0
+  if (inputYear % 400 === 0) {
+    return true;
+  }
+  else if (inputYear % 100 === 0) {
+    return false;
+  }
+  else if (inputYear % 4 === 0) {
+    return true;
+  }
+  else if (!inputYear) {
+    throw new Error('Must provide an argument!')
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = year;
--- Functions/Year/year.test.js
+++ Functions/Year/year.test.js
@@ -3,10 +3,35 @@ const year = require('./year');
 
 describe('the year function', () => {
 
-  // Happy Path test:
-  test('should take year and determine if it is a leap year', () => {
+  // If year divisible by 4
+  test('should return true, divisible by 4', () => {
     const result = year(1988);
 
     expect(result).toBe(true);
   });
+
+
+  // If year divisible by 100
+  test('should return false, divisible by 100', () => {
+    const result = year(1900);
+
+    expect(result).toBe(false);
+  });
+
+
+  // If year divisible by 400
+  test('should return true, divisible by 400', () => {
+    const result = year(2000);
+
+    expect(result).toBe(true);
+  });
+
+
+  // If year not given, throw error
+  test('should throw error, year not given', () => {
+    expect(() => {
+      year();
+    }).toThrow('Must provide an argument!')
+  });
+
 })
